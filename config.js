b7f8ac91860235b53c1dc45d00eb4bd2dcc31d0d
dreamforen <dreamforen1@naver.com>
2026-09-14 14:12:25 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,5 @@
 window.DREAMFOREN_CONFIG = Object.freeze({
   supabaseUrl: "https://oejgzgvozlitqjasobol.supabase.co",
   supabasePublishableKey: "sb_publishable_xx0F9HfDd3T72jPH7T0xHA_JU5Ark6b"
+  kakaoJavaScriptKey: '8f7af32ca8c088e2e5f2b4a91e3d3521'
 });
